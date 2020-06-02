5c71b406fabf76f498751a18e5af9c096119c4d9
hakimamg <hakimdu9347@gmail.com>
2020-06-02 15:38:33 +0200

    set folders and configure project

---
+++ ReactivePictures/CoreLayer/Network/APIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.swift
+//  ReactivePictures
+//
+//  Created by Amg on 02/06/2020.
+//  Copyright © 2020 Amg-Gauthier. All rights reserved.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let apiKey = "RQWV50QT3R3p4yBebcsPB2w26nf_NFTq2IzhsLKS9Eg"
+}
