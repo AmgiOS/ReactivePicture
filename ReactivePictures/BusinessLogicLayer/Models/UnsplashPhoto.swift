a737a3cddc764c17400e1e3a3b893583ff85296c
hakimamg <hakimdu9347@gmail.com>
2020-06-03 14:49:53 +0200

    implemented unsplash photo codable for request

---
+++ ReactivePictures/BusinessLogicLayer/Models/UnsplashPhoto.swift
@@ -0,0 +1,34 @@
+//
+//  UnsplashPhoto.swift
+//  ReactivePictures
+//
+//  Created by Amg on 03/06/2020.
+//  Copyright © 2020 Amg-Gauthier. All rights reserved.
+//
+
+import Foundation
+
+struct UnsplashPhoto: Codable {
+    let id: String?
+    let description: String?
+    let altDescription: String?
+    let urls: Urls?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, description
+        case altDescription = "alt_description"
+        case urls
+    }
+}
+
+struct Urls: Codable {
+    let raw: String?
+    let full: String?
+    let regular: String?
+    let small: String?
+    let thumb: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case raw, full, regular, small, thumb
+    }
+}
---
+++ ReactivePictures/CoreLayer/Network/NetworkError.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkError.swift
+//  ReactivePictures
+//
+//  Created by Amg on 03/06/2020.
+//  Copyright © 2020 Amg-Gauthier. All rights reserved.
+//
+
+import Foundation
